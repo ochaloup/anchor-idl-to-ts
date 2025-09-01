3ba199305b9ee446696323b294bc7a332aeb2db8
Ondra Chaloupka <chalda@marinade.finance>
2025-09-01 18:11:31 +0200

    anchor version update

---
+++ src/helpers.rs
@@ -0,0 +1,64 @@
+use anchor_idl::{IdlDefinedFields, IdlGenericArg, IdlInstructionAccountItem, IdlType};
+use heck::ToLowerCamelCase;
+
+pub(crate) fn convert_instruction_account_item(item: &mut IdlInstructionAccountItem) {
+    match item {
+        IdlInstructionAccountItem::Composite(composite) => {
+            composite.name = composite.name.to_lower_camel_case();
+            for nested_account in composite.accounts.iter_mut() {
+                convert_instruction_account_item(nested_account);
+            }
+        }
+        IdlInstructionAccountItem::Single(single) => {
+            single.name = single.name.to_lower_camel_case();
+        }
+    }
+}
+
+// Helper function to convert field names in IdlDefinedFields
+pub(crate) fn convert_defined_fields(fields: &mut IdlDefinedFields) {
+    match fields {
+        IdlDefinedFields::Named(named_fields) => {
+            for field in named_fields.iter_mut() {
+                field.name = field.name.to_lower_camel_case();
+                convert_idl_type_names(&mut field.ty);
+            }
+        }
+        IdlDefinedFields::Tuple(tuple_types) => {
+            for ty in tuple_types.iter_mut() {
+                convert_idl_type_names(ty);
+            }
+        }
+    }
+}
+
+// Helper function to convert names within IdlType recursively
+pub(crate) fn convert_idl_type_names(idl_type: &mut IdlType) {
+    match idl_type {
+        IdlType::Option(inner) => {
+            convert_idl_type_names(inner);
+        }
+        IdlType::Vec(inner) => {
+            convert_idl_type_names(inner);
+        }
+        IdlType::Array(inner, _) => {
+            convert_idl_type_names(inner);
+        }
+        IdlType::Defined { name, generics } => {
+            *name = name.to_lower_camel_case();
+
+            for generic in generics.iter_mut() {
+                match generic {
+                    IdlGenericArg::Type { ty } => {
+                        convert_idl_type_names(ty);
+                    }
+                    IdlGenericArg::Const { .. } => {}
+                }
+            }
+        }
+        IdlType::Generic(name) => {
+            *name = name.to_lower_camel_case();
+        }
+        _ => {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use anchor_idl::Idl;
+mod helpers;
+use helpers::{convert_defined_fields, convert_idl_type_names, convert_instruction_account_item};
+
+use anchor_idl::IdlTypeDefTy;
+use anchor_idl::{Idl, IdlTypeDefGeneric};
 use anyhow::{Context, Result};
 use clap::Parser;
 use heck::{ToLowerCamelCase, ToUpperCamelCase};
@@ -12,44 +16,108 @@ struct Cli {
     #[clap(short, long)]
     outdir: Option<std::path::PathBuf>,
 
-    /// Name of the IDL type and exported const to generate. Defaults to the self-defined IDL name.
+    /// Name of the IDL type and exported const to generate. Defaults to the self-defined IDL name in metadata.
     #[clap(short = 'n', long)]
-    program_name: Option<String>,
+    idl_type_name: Option<String>,
+
+    /// Additional metadata info added to the generated file.
+    description: Option<String>,
+
+    // Addtional metadata info on url to repository to be added to the generated file.
+    repository: Option<String>,
 }
 
-pub fn idl_ts(idl: &mut Idl, name_arg: Option<String>) -> Result<String, std::io::Error> {
+pub fn idl_ts(idl: &mut Idl, idl_type_name: Option<String>) -> Result<String, std::io::Error> {
+    // --- Accounts
     for acc in idl.accounts.iter_mut() {
         acc.name = acc.name.to_lower_camel_case();
     }
+    // --- Events
+    for event in idl.events.iter_mut() {
+        event.name = event.name.to_lower_camel_case();
+    }
+    // --- Errors
+    for error in idl.errors.iter_mut() {
+        error.name = error.name.to_lower_camel_case();
+    }
+    // --- Types
+    for type_def in idl.types.iter_mut() {
+        type_def.name = type_def.name.to_lower_camel_case();
+        match &mut type_def.ty {
+            IdlTypeDefTy::Struct { fields } => {
+                if let Some(fields) = fields {
+                    convert_defined_fields(fields);
+                }
+            }
+            IdlTypeDefTy::Enum { variants } => {
+                for variant in variants.iter_mut() {
+                    variant.name = variant.name.to_lower_camel_case();
+                    if let Some(fields) = &mut variant.fields {
+                        convert_defined_fields(fields);
+                    }
+                }
+            }
+            IdlTypeDefTy::Type { alias } => {
+                convert_idl_type_names(alias);
+            }
+        }
+        for generic in type_def.generics.iter_mut() {
+            match generic {
+                IdlTypeDefGeneric::Type { name } => {
+                    *name = name.to_lower_camel_case();
+                }
+                IdlTypeDefGeneric::Const { name, .. } => {
+                    *name = name.to_lower_camel_case();
+                }
+            }
+        }
+    }
+    // --- Constants
+    for constant in idl.constants.iter_mut() {
+        constant.name = constant.name.to_lower_camel_case();
+    }
+    // --- Instructions
+    for instruction in idl.instructions.iter_mut() {
+        instruction.name = instruction.name.to_lower_camel_case();
+        for arg in instruction.args.iter_mut() {
+            arg.name = arg.name.to_lower_camel_case();
+            convert_idl_type_names(&mut arg.ty);
+        }
+        for account in instruction.accounts.iter_mut() {
+            convert_instruction_account_item(account);
+        }
+    }
+
     let idl_json = serde_json::to_string_pretty(&idl)?;
-
-    let idl_name = name_arg.unwrap_or_else(|| idl.name.to_upper_camel_case());
-
-    let type_name = format!("{idl_name}IDLType");
-    let idl_const_name = format!("{idl_name}IDL");
-
+    let idl_name = idl_type_name.unwrap_or_else(|| idl.metadata.name.to_upper_camel_case());
+    let type_name = format!("{idl_name}");
     let type_export = format!("export type {type_name} = {idl_json};");
-    let const_export = format!("export const {idl_const_name}: {type_name} = {idl_json};");
 
-    Ok(format!("{type_export}\n\n{const_export}\n"))
+    Ok(format!("{type_export}\n"))
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     let bytes = std::fs::read(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
     let mut idl: Idl = serde_json::from_slice(&bytes).expect("Invalid IDL format.");
-    let ts_idl = idl_ts(&mut idl, args.program_name)?;
+
+    if let Some(description) = args.description {
+        idl.metadata.description = Some(description);
+    }
+    if let Some(repository) = args.repository {
+        idl.metadata.repository = Some(repository);
+    }
+
+    let ts_idl = idl_ts(&mut idl, args.idl_type_name)?;
 
     let mut default_path = args.path;
     default_path.pop();
 
-    let out = match args.outdir {
-        None => default_path,
-        Some(path) => path,
-    };
+    let out = args.outdir.unwrap_or_else(|| default_path);
 
-    let ts_out = out.join(&idl.name).with_extension("ts");
+    let ts_out = out.join(&idl.metadata.name).with_extension("ts");
 
     std::fs::write(&ts_out, ts_idl)?;
     println!("file created at: {}", ts_out.display());
